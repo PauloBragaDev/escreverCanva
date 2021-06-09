6dfebb29df25a55fb30795caed106a6ca16c6ece
Paulo Braga <paulohb.amorim@hotmail.com>
2021-06-09 10:45:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+  function clearCanvas(context, canvas) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var w = canvas.width;
+    canvas.width = 1;
+    canvas.width = w;
+  }
+
+
+    var Draw = {
+    obj : document.getElementById('canvas'),
+    contexto : document.getElementById('canvas').getContext("2d"),
+    _init:function(){
+        Draw.obj.onmousemove = Draw._over;
+        Draw.obj.onmousedown = Draw._ativa;
+        Draw.obj.onmouseup = Draw._inativa;
+        Draw.obj.onselectstart = function () { return false; };
+    },
+    _over:function(e){
+        if(!Draw.ativo) return;
+        Draw.contexto.beginPath();
+        Draw.contexto.lineTo(Draw.x,Draw.y);
+        Draw.contexto.lineTo(e.layerX, e.layerY);
+        Draw.contexto.stroke();
+        Draw.contexto.closePath();
+        Draw.x = e.layerX;
+        Draw.y = e.layerY;
+    },
+    _ativa:function(e){
+        Draw.ativo = true;
+        Draw.x = e.layerX;
+        Draw.y = e.layerY;
+    },
+    _inativa:function(){
+        Draw.ativo = false;
+    }   
+}
+Draw._init();
+
+
+document.getElementById('clear').addEventListener('click', function() {
+        var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+context.clearRect(0, 0, canvas.width, canvas.height);
+      }, false);
+
+  
